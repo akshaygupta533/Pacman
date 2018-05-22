9d3dd08930aff26a76a6bee5537b36a780000b7c
Akshay Gupta <akshay.gupta@research.iiit.ac.in>
2018-05-22 13:02:27 -0400

    Updated the Pacman maze

--- board.py
+++ board.py
@@ -5,14 +5,33 @@ class board:
     def __init__(self):
         self.ver = []
         self.hor = []
+       
+        #board frame
         self.hor.append(((pac_size,3*pac_size),(board_width-pac_size,3*pac_size)))
         self.hor.append(((pac_size,board_height-pac_size),(board_width-pac_size,board_height - pac_size)))
-        self.ver.append(((pac_size,3*pac_size),(pac_size,board_height - pac_size)))
-        self.ver.append(((board_width-pac_size,3*pac_size),(board_width - pac_size,board_height - pac_size)))
+       
+        self.ver.append(((pac_size,3*pac_size),(pac_size,board_height/2-0.75*pac_size)))
+        self.ver.append(((pac_size,board_height/2+0.75*pac_size),(pac_size,board_height - pac_size)))
+        
+        self.ver.append(((board_width-pac_size,3*pac_size),(board_width - pac_size,board_height/2-0.75*pac_size)))
+        self.ver.append(((board_width-pac_size,board_height/2+0.75*pac_size),(board_width - pac_size,board_height - pac_size)))
+       
+        self.hor.append(((pac_size,board_height/2-0.75*pac_size-line_thickness/2),(5*pac_size,board_height/2-0.75*pac_size-line_thickness/2)))
+        self.hor.append(((pac_size,board_height/2+0.75*pac_size+line_thickness/2-1),(5*pac_size,board_height/2+0.75*pac_size+line_thickness/2-1)))
+
+        self.hor.append(((board_width-5*pac_size,board_height/2-0.75*pac_size-line_thickness/2),(board_width-pac_size,board_height/2-0.75*pac_size-line_thickness/2)))
+        self.hor.append(((board_width-5*pac_size,board_height/2+0.75*pac_size+line_thickness/2-1),(board_width-pac_size,board_height/2+0.75*pac_size+line_thickness/2-1)))
+
+       
+        #center box for aliens
         self.hor.append(((board_width/2 - 2.5*pac_size,board_height/2),(board_width/2 + 2.5*pac_size,board_height/2)))
         self.hor.append(((board_width/2 - 2.5*pac_size,board_height/2+2*pac_size),(board_width/2 + 2.5*pac_size,board_height/2+2*pac_size)))
         self.ver.append(((board_width/2 - 2.5*pac_size,board_height/2-line_thickness/2+1),(board_width/2 - 2.5*pac_size,board_height/2+2*pac_size+line_thickness/2)))
         self.ver.append(((board_width/2 + 2.5*pac_size,board_height/2-line_thickness/2+1),(board_width/2 + 2.5*pac_size,board_height/2+2*pac_size+line_thickness/2)))
+        
+        #other lines
+        self.ver.append(((0.667*board_width,board_height-4*pac_size),(0.667*board_width,board_height - pac_size)))
+        self.ver.append(((0.333*board_width,board_height-4*pac_size),(0.333*board_width,board_height - pac_size)))
 
     def draw(self):
         for vlines in self.ver:
--- main.py
+++ main.py
@@ -42,10 +42,18 @@ while True:
         if pacman.x<hlines[1][0]+pac_size/2 and pacman.x>hlines[0][0]-pac_size/2:
             if pacman.y+y_change<hlines[0][1]+(pac_size+line_thickness)/2 and pacman.y+y_change>hlines[0][1]-(pac_size+line_thickness)/2:
                 y_change=0
+    
+        if pacman.y<hlines[0][1]+(line_thickness+pac_size)/2 and pacman.y>hlines[0][1]-(line_thickness+pac_size)/2:
+            if pacman.x+x_change<hlines[1][0]+pac_size/2 and pacman.x+x_change>hlines[0][0]-pac_size/2:
+                x_change=0
+    
     for vlines in brd.ver:
         if pacman.y<vlines[1][1]+pac_size/2 and pacman.y>vlines[0][1]-pac_size/2:
             if pacman.x+x_change<vlines[0][0]+(pac_size+line_thickness)/2 and pacman.x+x_change>vlines[0][0]-(pac_size+line_thickness)/2:
                 x_change=0
+        if pacman.x<vlines[0][0]+(line_thickness+pac_size)/2 and pacman.x>vlines[0][0]-(pac_size+line_thickness)/2:
+            if pacman.y+y_change<vlines[1][1]+pac_size/2 and pacman.y+y_change>vlines[0][1]-pac_size/2:
+                y_change=0
     
     
 
